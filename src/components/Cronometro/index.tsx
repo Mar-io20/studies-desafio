b7f7be0ef56267a2f44415147cd7203457aa0b4c
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-08 17:31:04 -0300

    convertendo tudo pra segundo

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,12 +2,25 @@ import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss'
 import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
-export function Cronometro() {
-  console.log('conversao: ', tempoParaSegundos('01:01:01'))
+interface Props {
+  selecionado: ITarefa | undefined
+}
+
+export function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if(selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado?.tempo));
+    }
+  }, [selecionado]) 
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: {tempo}
       <div className={style.relogioWrapper}>
           <Relogio/>
       </div>
