3c61f9a2310ed1644b8b56a907d719ef595adfac
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-08 08:36:14 -0300

    iniciando o dia

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 class Botao extends React.Component {
     render() {
+        const estaAtivo = true;
+        const styles = {
+            backgroundColor: estaAtivo ? "green" : "red"
+        }
         return (
-            <button>
+            <button style={styles}>
                 botâo
             </button>
         )
