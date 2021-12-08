f28df383122cd2c25f77a109e33cf72b33b8d683
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-08 09:49:49 -0300

    aprendi bastante sobre props e reutilizacao d comp

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import style from './Botao.module.scss';
 
-
+ 
 class Botao extends React.Component {
   render() {
     return (
         <button className={style.botao}>
-            botâo
+            {this.props.children}
         </button>
     )
   }
