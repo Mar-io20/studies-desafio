cc306dfdae17f9b9ae1b4822402077c27cd6bab9
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-08 09:32:12 -0300

    mudando alguns estilos para scss module

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './style.scss'
+import style from './Botao.module.scss';
+
 
 class Botao extends React.Component {
   render() {
     return (
-        <button className="botao">
+        <button className={style.botao}>
             botâo
         </button>
     )
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Botao from "../Botao";
-import "./style.scss";
+import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form className="novaTarefa">
-        <div className="inputContainer">
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
@@ -16,7 +16,7 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <div className="inputContainer">
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">Tempo</label>
           <input
             type="time"
