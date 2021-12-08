5791f8dbf216cd936e2444266b12e60acbaa8714
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-08 16:46:24 -0300

    Adicionando a classe para style selecionado

--- src/components/Lista/item/index.tsx
+++ src/components/Lista/item/index.tsx
@@ -16,7 +16,9 @@ export default function Item (
     } : Props) {
     console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
     return (
-        <li className={style.item} onClick={() => selecionaTarefa({
+        <li 
+        className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} 
+        onClick={() => selecionaTarefa({
             tarefa, 
             tempo, 
             selecionado, 
