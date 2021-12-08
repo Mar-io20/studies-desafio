c0f36cb03b00c64562962aea46360197c46ad395
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-08 14:13:24 -0300

    Entendendo porque o react nao e reativo

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./item";
 import style from "./Lista.module.scss";
 
 function Lista() {
-  const tarefas = [
-    {
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "JavaScript",
-      tempo: "01:00:00",
-    },
-    {
-      tarefa: "Typescript",
-      tempo: "03:00:00",
-    },
-  ];
+  const [tarefas, setTarefas] = useState([{
+    tarefa: "React",
+    tempo: "02:00:00",
+  },
+  {
+    tarefa: "JavaScript",
+    tempo: "01:00:00",
+  },
+  {
+    tarefa: "Typescript",
+    tempo: "03:00:00",
+  }]);
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estududos do dia </h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])
+      }}>Estududos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 
