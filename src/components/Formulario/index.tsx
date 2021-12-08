b06c84371c6f4965c15c2be5ff18ff3ec53636c1
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-08 15:02:57 -0300

    trabalhando com useState e components

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from "./Formulario.module.scss";
 
-class Formulario extends React.Component {
+class Formulario extends React.Component<{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}> {
   state = {
     tarefa: "",
     tempo: "00:00",
@@ -10,7 +13,7 @@ class Formulario extends React.Component {
 
   adicionarTarefa(evento: React.FocusEvent<HTMLFormElement>) {
     evento.preventDefault();
-    console.log("state: ", this.state);
+    this.props.setTarefas(tarefasAntigas => [...tarefasAntigas,{...this.state}])
   }
 
   render() {
@@ -42,7 +45,7 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <Botao>Adicionar</Botao>
+        <Botao type="submit">Adicionar</Botao>
       </form>
     );
   }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
+import { ITarefa } from "../../types/tarefa";
 import Item from "./item";
 import style from "./Lista.module.scss";
 
-function Lista() {
-  const [tarefas, setTarefas] = useState([{
-    tarefa: "React",
-    tempo: "02:00:00",
-  },
-  {
-    tarefa: "JavaScript",
-    tempo: "01:00:00",
-  },
-  {
-    tarefa: "Typescript",
-    tempo: "03:00:00",
-  }]);
+
+function Lista( {tarefas }: { tarefas: ITarefa[]}) {
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])
-      }}>Estududos do dia </h2>
+      <h2>Estududos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 
